52778004f2fb2c953ad10ac9911e92bae94b90c6
creativitashop <creativita.110@gmail.com>
2026-03-15 01:51:54 -0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -9,11 +9,11 @@
 
     f.id = 'chat-f';
     f.src = 'https://creativitashop.github.io/mi-bot-creativita/';
-    // Altura de 600px para que se vea completo el diseño
-    f.style.cssText = 'width:360px;height:600px;border:none;border-radius:20px;box-shadow:0 10px 40px rgba(0,0,0,0.5);display:none;background:white;margin-bottom:15px;';
+    // LE SUBÍ LA ALTURA A 650 PARA QUE NO SE VEA MOCHO
+    f.style.cssText = 'width:360px;height:650px;border:none;border-radius:20px;box-shadow:0 10px 40px rgba(0,0,0,0.5);display:none;background:white;margin-bottom:15px;';
 
     b.innerHTML = '💬';
-    b.style.cssText = 'background:#D4AF37;border:none;border-radius:50%;width:70px;height:70px;cursor:pointer;box-shadow:0 6px 20px rgba(0,0,0,0.4);display:flex;align-items:center;justify-content:center;color:white;font-size:35px;transition:0.3s;';
+    b.style.cssText = 'background:#D4AF37;border:none;border-radius:50%;width:75px;height:75px;cursor:pointer;box-shadow:0 6px 20px rgba(0,0,0,0.4);display:flex;align-items:center;justify-content:center;color:white;font-size:35px;';
 
     b.onclick = function() {
         var el = document.getElementById('chat-f');
